855704bb320af69fde66a46a09433b21db8b5e09
Luiz Guilherme <xenubiagames@gmail.com>
2024-03-16 14:05:32 -0300

    feat: create empresas

--- src/Controller/EmpresaController.php
+++ src/Controller/EmpresaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Empresa;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,13 +10,27 @@ use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 
+#[Route('/api', name: 'api_')]
 class EmpresaController extends AbstractController
 {
-    public function index(): JsonResponse
+    #[Route('/empresas', name: 'empresa_create', methods:['post'])]
+    public function create(ManagerRegistry $doctrine, Request $request): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/EmpresaController.php',
-        ]);
+        $entityManager = $doctrine->getManager();
+
+        $empresa = new Empresa();
+        $empresa->setName($request->request->get('name'));
+        $empresa->setEmail($request->request->get('email'));
+
+        $entityManager->persist($empresa);
+        $entityManager->flush();
+
+        $data = [
+            'id' => $empresa->getId(),
+            'name' => $empresa->getName(),
+            'email' => $empresa->getEmail(),
+        ];
+
+        return $this->json($data);
     }
 }
