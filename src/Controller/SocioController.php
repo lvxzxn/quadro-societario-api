dea3e441df3f03dd6082387f0f6797d53f8b585a
Luiz Guilherme <xenubiagames@gmail.com>
2024-03-17 21:44:55 -0300

    style: format

--- src/Controller/SocioController.php
+++ src/Controller/SocioController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Empresa;
 use App\Entity\Socio;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,8 +15,8 @@ use Symfony\Component\HttpFoundation\Request;
 #[Route('/api', name: 'api_')]
 class SocioController extends AbstractController
 {
-    #[Route('/socios', name: 'socios_index', methods:['get'])]
-    public function index(ManagerRegistry $doctrine): JsonResponse 
+    #[Route('/socios', name: 'socios_index', methods: ['get'])]
+    public function index(ManagerRegistry $doctrine): JsonResponse
     {
         $socios = $doctrine
             ->getRepository(Socio::class)
@@ -35,20 +37,22 @@ class SocioController extends AbstractController
         return $this->json($data);
     }
 
-    #[Route('/socios', name: 'socios_create', methods:['POST'])]
+    #[Route('/socios', name: 'socios_create', methods: ['POST'])]
     public function create(ManagerRegistry $doctrine, Request $request): JsonResponse
     {
         $entityManager = $doctrine->getManager();
+    
+        $cpf = $request->request->get('cpf');
 
         $socio = new Socio();
         $socio->setName($request->request->get('name'));
         $socio->setEmail($request->request->get('email'));
-        $socio->setCPF($request->request->get('cpf'));
+        $socio->setCPF($cpf); 
         $socio->setEmpresa($request->request->get('empresa_id'));
-
+    
         $entityManager->persist($socio);
         $entityManager->flush();
-
+    
         $data = [
             'id' => $socio->getId(),
             'name' => $socio->getName(),
@@ -56,24 +60,24 @@ class SocioController extends AbstractController
             'cpf' => $socio->getCPF(),
             'empresa_id' => $socio->getEmpresa(),
         ];
-
+    
         return $this->json($data);
     }
 
-    
-    #[Route('/socios/{id}', name: 'socios_delete', methods:['delete'])]
-    public function delete(ManagerRegistry $doctrine, int $id): JsonResponse 
+
+    #[Route('/socios/{id}', name: 'socios_delete', methods: ['delete'])]
+    public function delete(ManagerRegistry $doctrine, int $id): JsonResponse
     {
         $entityManager = $doctrine->getManager();
         $socio = $entityManager->getRepository(Socio::class)->find($id);
 
-        if (!$socio){
+        if (!$socio) {
             return $this->json('Nenhum sócio foi encontrado com o id ' . $id, 404);
         }
 
         $entityManager->remove($socio);
         $entityManager->flush();
 
-        return $this->json('O sócio de id '. $id . ' foi deletado com sucesso.');
+        return $this->json('O sócio de id ' . $id . ' foi deletado com sucesso.');
     }
 }
